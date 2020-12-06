743017b32317efe4735e2e00de1681791c37b664
evanchooly <jlee@antwerkz.com>
2020-12-05 21:50:07 -0500

    clean up

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -1,9 +1,8 @@
-use std::cmp::{max, min};
+use std::cmp::max;
 use std::collections::BTreeSet;
 use std::fs;
 
 fn main() {
-    // read_passes("input/sample.input");
     read_passes("input/actual.input");
 }
 
@@ -11,42 +10,26 @@ fn read_passes(input: &str) {
     let contents = fs::read_to_string(input)
         .expect("Something went wrong reading the file");
     let mut max_id = -100;
-    let mut min_row = 100000;
-    let mut max_row = -100;
     let mut ids = BTreeSet::new();
-    let open = ' ';
-    let taken = 'X';
-    let mut array: [[char; 8]; 127] = [[open; 8]; 127];
 
     for line in contents.lines() {
         if line != "" {
-            let (row, column, seat_id) = process(line);
-            array[row as usize][column as usize] = taken;
+            let (_row, _column, seat_id) = process(line);
             max_id = max(max_id, seat_id);
-            max_row = max(max_row, row);
-            min_row = min(min_row, row);
             ids.insert(seat_id);
-            // println!("line: {}, row : {} , column : {}, seat_id: {}", line, row, column, seat_id);
         }
     }
 
     let mut last_id = -1;
+    let mut missing_id = -1;
 
     for id in ids {
         if (last_id != -1) && (id != (last_id + 1)) {
-            println!("missing id: {}", id)
+            missing_id = id - 1;
         }
         last_id = id
     }
-/*    for row in &array {
-        for col in row {
-            print!("{}", col)
-        }
-        println!();
-    }
-    println!();
-*/
-    println!("max seat_id: {}, max row: {}, min row: {}", max_id, max_row, min_row);
+    println!("max seat_id: {}, missing ID: {}", max_id, missing_id);
 }
 
 fn process(line: &str) -> (i32, i32, i32) {
@@ -75,16 +58,9 @@ fn process(line: &str) -> (i32, i32, i32) {
             }
             _ => {}
         }
-        // println!("letter : {}, row : ({}, {}) , column : ({}, {})", letter, row_range.0, row_range.1,
-        //          column_range.0, column_range.1);
     }
 
-    let seat_id = row * 8 + column;
-    return (row, column, seat_id)
-}
-
-fn _show(name: &str, tuple: (i32, i32)) {
-    println!("{}.0 : {}, {}.1: {}", name, tuple.0, name, tuple.1);
+    return (row, column, row * 8 + column)
 }
 
 fn upper(range: (i32, i32)) -> (i32, i32) {
